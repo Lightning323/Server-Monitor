50cf3e5f734ab84cedd2188f13eaa32c87613a3f
Lightning323 <sam.p.w524@gmail.com>
2026-06-19 12:31:19 -0600

    update

--- src/main/java/org/lightning/serverMonitor/javalinWebapp/packets/SensorHistoryRequestPacket.java
+++ src/main/java/org/lightning/serverMonitor/javalinWebapp/packets/SensorHistoryRequestPacket.java
@@ -10,9 +10,10 @@ public record SensorHistoryRequestPacket
         (String sensor, long startDate, long endDate) implements WsPacket {
 
     final static int CHUNK_SIZE = 1500;
+    final static int INTERVAL_MS = 1000;
 
     public void handle(PacketContext ctx) {
-        List<SensorDatabase.HistoryEntry> data = SensorDatabase.getSensorDataRange(sensor, startDate, endDate, 2000);
+        List<SensorDatabase.HistoryEntry> data = SensorDatabase.getSensorDataRange(sensor, startDate, endDate, INTERVAL_MS);
         System.out.println("Sensor data range for " + sensor + ": " + data.size());
         if (data.size() == 0) {
             ctx.send(new SensorHistoryPacket(sensor, 0, data.toArray(new SensorDatabase.HistoryEntry[0]), true));
